47fbfbb089095566fd2cafee91f462c1f8a81db7
Serafim Prilezhaev <simkaa7@gmail.com>
2024-05-21 15:21:25 +0300

    Parameters page was completed

--- VissmaFlow.View/UserControls/Keyboard/VirtualKeyWidthMultiplayer.cs
+++ VissmaFlow.View/UserControls/Keyboard/VirtualKeyWidthMultiplayer.cs
@@ -7,7 +7,8 @@ namespace VissmaFlow.View.UserControls.Keyboard
     public class VirtualKeyWidthMultiplayer : IValueConverter
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {            
+        {
+            if (value is null || parameter is null) return 0.0f;
             var v = double.Parse(value.ToString());
             var p = double.Parse(parameter.ToString());
             return v * (p / 10.0);
